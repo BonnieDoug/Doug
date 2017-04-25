f218dec8e6e4fa16d2b8f1189db9f9fba849038c
Doug <Doug@bonniechef.com>
2017-04-25 16:43:07 +0100

    Finish entity mapping

---
+++ src/AppBundle/Entity/Category.php
@@ -0,0 +1,251 @@
+<?php
+/**
+ * User: dhayward
+ * Date: 25/04/17
+ * Time: 15:58
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="categories")
+ * @ORM\Entity()
+ */
+class Category
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column()
+     * @ORM\GeneratedValue()
+     */
+    private $id;
+
+    /** @ORM\Column(type="string", length=200) */
+    private $name;
+    /**
+     * Status flag for Category.
+     * Rather than a retired, locked, expired etc column, have put all in one and all tables can then share the same lookup table
+     * @ORM\ManyToOne(targetEntity="Status", inversedBy="categories")
+     * @ORM\JoinColumn(name="status_id", referencedColumnName="id")
+     */
+    private $status;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Category", inversedBy="children")
+     * @ORM\JoinColumn(name="parent_id", referencedColumnName="id")
+     */
+    private $parent;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Category", mappedBy="parent")
+     */
+    private $children;
+
+    /**
+     * Many Categories have Many Posts.
+     * @ORM\ManyToMany(targetEntity="Post", mappedBy="categories")
+     */
+    private $posts;
+
+    /**
+     * Many Categories have Many Images.
+     * @ORM\ManyToMany(targetEntity="Image", mappedBy="categories")
+     */
+    private $images;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->children = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->posts = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->images = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set status
+     *
+     * @param \AppBundle\Entity\Status $status
+     *
+     * @return Category
+     */
+    public function setStatus(\AppBundle\Entity\Status $status = null)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return \AppBundle\Entity\Status
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set parent
+     *
+     * @param \AppBundle\Entity\Category $parent
+     *
+     * @return Category
+     */
+    public function setParent(\AppBundle\Entity\Category $parent = null)
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
+
+    /**
+     * Get parent
+     *
+     * @return \AppBundle\Entity\Category
+     */
+    public function getParent()
+    {
+        return $this->parent;
+    }
+
+    /**
+     * Add child
+     *
+     * @param \AppBundle\Entity\Category $child
+     *
+     * @return Category
+     */
+    public function addChild(\AppBundle\Entity\Category $child)
+    {
+        $this->children[] = $child;
+
+        return $this;
+    }
+
+    /**
+     * Remove child
+     *
+     * @param \AppBundle\Entity\Category $child
+     */
+    public function removeChild(\AppBundle\Entity\Category $child)
+    {
+        $this->children->removeElement($child);
+    }
+
+    /**
+     * Get children
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getChildren()
+    {
+        return $this->children;
+    }
+
+    /**
+     * Add post
+     *
+     * @param \AppBundle\Entity\Post $post
+     *
+     * @return Category
+     */
+    public function addPost(\AppBundle\Entity\Post $post)
+    {
+        $this->posts[] = $post;
+
+        return $this;
+    }
+
+    /**
+     * Remove post
+     *
+     * @param \AppBundle\Entity\Post $post
+     */
+    public function removePost(\AppBundle\Entity\Post $post)
+    {
+        $this->posts->removeElement($post);
+    }
+
+    /**
+     * Get posts
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getPosts()
+    {
+        return $this->posts;
+    }
+
+    /**
+     * Add image
+     *
+     * @param \AppBundle\Entity\Image $image
+     *
+     * @return Category
+     */
+    public function addImage(\AppBundle\Entity\Image $image)
+    {
+        $this->images[] = $image;
+
+        return $this;
+    }
+
+    /**
+     * Remove image
+     *
+     * @param \AppBundle\Entity\Image $image
+     */
+    public function removeImage(\AppBundle\Entity\Image $image)
+    {
+        $this->images->removeElement($image);
+    }
+
+    /**
+     * Get images
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getImages()
+    {
+        return $this->images;
+    }
+}
--- src/AppBundle/Entity/Status.php
+++ src/AppBundle/Entity/Status.php
@@ -1,16 +1,259 @@
 <?php
 
 namespace AppBundle\Entity;
+
 /**
  * User: doug
  * Date: 4/24/17
  * Time: 11:39 PM
  */
+use Doctrine\ORM\Mapping as ORM;
 
-
+/**
+ * @ORM\Table(name="status")
+ * @ORM\Entity()
+ */
 class Status
 {
+    /**
+     * @ORM\Id
+     * @ORM\Column()
+     * @ORM\GeneratedValue()
+     */
     private $id;
+
+    /** @ORM\Column(type="string", length=200) */
     private $name;
+
+    /**
+     * Status flag for Status.
+     * The one place where retired is used instead of status, as really they can only be on/off and a join could cause issues long term.
+     * @ORM\Column(type="boolean")
+     */
     private $retired;
+
+    /**
+     * @ORM\OneToMany(targetEntity="User", mappedBy="status")
+     */
+    private $users;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Comment", mappedBy="status")
+     */
+    private $comments;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Comment", mappedBy="status")
+     */
+    private $posts;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Comment", mappedBy="status")
+     */
+    private $images;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->users = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->comments = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->posts = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->images = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Status
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set retired
+     *
+     * @param boolean $retired
+     *
+     * @return Status
+     */
+    public function setRetired($retired)
+    {
+        $this->retired = $retired;
+
+        return $this;
+    }
+
+    /**
+     * Get retired
+     *
+     * @return boolean
+     */
+    public function getRetired()
+    {
+        return $this->retired;
+    }
+
+    /**
+     * Add user
+     *
+     * @param \AppBundle\Entity\User $user
+     *
+     * @return Status
+     */
+    public function addUser(\AppBundle\Entity\User $user)
+    {
+        $this->users[] = $user;
+
+        return $this;
+    }
+
+    /**
+     * Remove user
+     *
+     * @param \AppBundle\Entity\User $user
+     */
+    public function removeUser(\AppBundle\Entity\User $user)
+    {
+        $this->users->removeElement($user);
+    }
+
+    /**
+     * Get users
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getUsers()
+    {
+        return $this->users;
+    }
+
+    /**
+     * Add comment
+     *
+     * @param \AppBundle\Entity\Comment $comment
+     *
+     * @return Status
+     */
+    public function addComment(\AppBundle\Entity\Comment $comment)
+    {
+        $this->comments[] = $comment;
+
+        return $this;
+    }
+
+    /**
+     * Remove comment
+     *
+     * @param \AppBundle\Entity\Comment $comment
+     */
+    public function removeComment(\AppBundle\Entity\Comment $comment)
+    {
+        $this->comments->removeElement($comment);
+    }
+
+    /**
+     * Get comments
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getComments()
+    {
+        return $this->comments;
+    }
+
+    /**
+     * Add post
+     *
+     * @param \AppBundle\Entity\Comment $post
+     *
+     * @return Status
+     */
+    public function addPost(\AppBundle\Entity\Comment $post)
+    {
+        $this->posts[] = $post;
+
+        return $this;
+    }
+
+    /**
+     * Remove post
+     *
+     * @param \AppBundle\Entity\Comment $post
+     */
+    public function removePost(\AppBundle\Entity\Comment $post)
+    {
+        $this->posts->removeElement($post);
+    }
+
+    /**
+     * Get posts
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getPosts()
+    {
+        return $this->posts;
+    }
+
+    /**
+     * Add image
+     *
+     * @param \AppBundle\Entity\Comment $image
+     *
+     * @return Status
+     */
+    public function addImage(\AppBundle\Entity\Comment $image)
+    {
+        $this->images[] = $image;
+
+        return $this;
+    }
+
+    /**
+     * Remove image
+     *
+     * @param \AppBundle\Entity\Comment $image
+     */
+    public function removeImage(\AppBundle\Entity\Comment $image)
+    {
+        $this->images->removeElement($image);
+    }
+
+    /**
+     * Get images
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getImages()
+    {
+        return $this->images;
+    }
 }
